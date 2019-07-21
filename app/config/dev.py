7197503f49be52466c77674b2a627ee3edde1ed4
Moreal <dev.moreal@gmail.com>
2019-07-21 21:40:20 +0900

    add: a config class

---
+++ app/config/dev.py
@@ -0,0 +1,18 @@
+class DevConfig():
+    HOST = 'localhost'
+    PORT = 8000
+    DEBUG = True
+
+    RUN_SETTING = dict(**{
+        'host': HOST,
+        'port': PORT,
+        'debug': DEBUG
+    })
+
+    API_VERSION = '1.0.0'
+    API_TITLE = 'Saucewich API'
+    API_DESCRIPTION = 'Saucewich data-server API'
+    API_PRODUCES_CONTENT_TYPES = ['application/json']
+    API_CONTACT_EMAIL = 'dev.moreal@gmail.com'
+
+    API_URI_FILTER = 'slash'
