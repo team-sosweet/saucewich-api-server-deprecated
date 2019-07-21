81364d49f06906a334add62ce4f15ae29b80a9a6
Moreal <dev.moreal@gmail.com>
2019-07-21 22:21:26 +0900

    refactor: view /user

--- app/views/api/user.py
+++ app/views/api/user.py
@@ -1,16 +1,19 @@
 from aiopeewee import model_to_dict
 from sanic import Blueprint
+from sanic.exceptions import abort
 from sanic.response import json
 from sanic.views import HTTPMethodView
 
-from app import User
+from app.models.user import User
+
 
 blueprint = Blueprint(__name__)
 
 class UserResource(HTTPMethodView):
-    def get(self, request, user_id: int):
+    async def get(self, request, user_id: int):
+        user = await User.get_or_404(User.id == user_id)
         return json(
-            await model_to_dict(User.get(user_id))
+            await model_to_dict(user)
         )
 
-blueprint.add_route(UserResource.as_view(), '/user')
+blueprint.add_route(UserResource.as_view(), '/user/<user_id>')
