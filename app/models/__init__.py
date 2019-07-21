f952d153be71228ff8014c394b4c10a0abeccf33
Moreal <dev.moreal@gmail.com>
2019-07-21 22:19:49 +0900

    add: custom methods into BaseModel
    aiopeewee is using old version peewee
    so we had to add methods for using the functions of latest version

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,6 @@
 from aiopeewee import AioMySQLDatabase, AioModel
+from peewee import DoesNotExist
+from sanic.exceptions import abort
 
 db = AioMySQLDatabase(
     'saucewich',
@@ -12,3 +14,21 @@ db = AioMySQLDatabase(
 class BaseModel(AioModel):
     class Meta:
         database = db
+
+    @classmethod
+    async def get_or_none(cls, *query, **filters):
+        try:
+            return await cls.get(*query, **filters)
+        except DoesNotExist:
+            pass
+
+    @classmethod
+    async def get_by_id(cls, pk):
+        return await cls.get(cls._meta.primary_key == pk)
+
+    @classmethod
+    async def get_or_404(cls, *query, **filters):
+        try:
+            return await cls.get(*query, **filters)
+        except DoesNotExist:
+            abort(404)
