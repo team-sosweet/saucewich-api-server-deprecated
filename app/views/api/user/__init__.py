4269452c900e752a811b6d75b5f40b21a60f05fd
Moreal <dev.moreal@gmail.com>
2019-07-24 17:02:54 +0900

    update: move and rename UserView

--- app/views/api/user/__init__.py
+++ app/views/api/user/__init__.py
@@ -1,27 +1,12 @@
 from sanic import Blueprint
-from sanic.response import json
-from sanic.views import HTTPMethodView
 
-from app.repositories.connections import MySQLConnection
-from app.repositories.user import UserRepository
-from app.services.user import UserService
+from app.views.api.user.information import UserInformationView
 from app.views.api.user.money import UserMoneyView
 from app.views.api.user.signup import UserSignupView
-
-
-class UserView(HTTPMethodView):
-    repository = UserRepository(MySQLConnection)
-    service = UserService(repository)
-
-    async def get(self, request, username: int):
-        user = await self.service.get(username)
-        del user['password']
-        return json(user)
-
 
 blueprint = Blueprint('user-api', '/user')
 
 blueprint.add_route(UserSignupView.as_view(), '/signup')
 
-blueprint.add_route(UserView.as_view(), '/<username>')
+blueprint.add_route(UserInformationView.as_view(), '/<username>')
 blueprint.add_route(UserMoneyView.as_view(), '/<username>/money')
---
+++ app/views/api/user/information.py
@@ -0,0 +1,15 @@
+from sanic.response import json
+from sanic.views import HTTPMethodView
+
+from app.repositories.connections import MySQLConnection
+from app.repositories.user import UserRepository
+from app.services.user import UserService
+
+class UserInformationView(HTTPMethodView):
+    repository = UserRepository(MySQLConnection)
+    service = UserService(repository)
+
+    async def get(self, request, username: int):
+        user = await self.service.get(username)
+        del user['password']
+        return json(user)
