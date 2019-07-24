d9cadb1f3bb3518b6608561b2953473098cfecd1
Moreal <dev.moreal@gmail.com>
2019-07-24 23:13:19 +0900

    add: UserFriendsView

--- app/views/api/user/__init__.py
+++ app/views/api/user/__init__.py
@@ -1,5 +1,6 @@
 from sanic import Blueprint
 
+from app.views.api.user.friend import UserFriendsView
 from app.views.api.user.information import UserInformationView
 from app.views.api.user.patch import UserPatchView
 from app.views.api.user.signup import UserSignupView
@@ -9,4 +10,5 @@ blueprint = Blueprint('user-api', '/user')
 blueprint.add_route(UserSignupView.as_view(), '/signup')
 
 blueprint.add_route(UserInformationView.as_view(), '/<username>')
+blueprint.add_route(UserFriendsView.as_view(), '/<username>/friends')
 blueprint.add_route(UserPatchView.as_view(), '/<username>/<field_name>')
---
+++ app/views/api/user/friend.py
@@ -0,0 +1,29 @@
+from sanic.exceptions import abort
+from sanic.response import json
+from sanic.views import HTTPMethodView
+
+from app.repositories.connections import MySQLConnection
+from app.repositories.friend import FriendRepository
+from app.repositories.user import UserRepository
+from app.services.friend import FriendService
+from app.services.user import UserService
+
+class UserFriendsView(HTTPMethodView):
+    user_repository = UserRepository(MySQLConnection)
+    user_service = UserService(user_repository)
+
+    friend_repository = FriendRepository(MySQLConnection)
+    friend_service = FriendService(friend_repository)
+
+    async def get(self, request, username: str):
+        user = await self.user_service.get(username)
+        return json({
+            'friends': await self.friend_service.get_all(user['seq'])
+        })
+
+    async def post(self, request, username: str):
+        user = await self.user_service.get(username)
+        if user['seq'] != request.json['user_id']:
+            abort(403)
+
+        await self.friend_service.create(request.json)
