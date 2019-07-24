95326fc4a4e8f649be5d68af8f39ebda722ba17d
Moreal <dev.moreal@gmail.com>
2019-07-24 17:15:03 +0900

    add: UserPointView view

--- app/views/api/user/__init__.py
+++ app/views/api/user/__init__.py
@@ -2,6 +2,7 @@ from sanic import Blueprint
 
 from app.views.api.user.information import UserInformationView
 from app.views.api.user.money import UserMoneyView
+from app.views.api.user.point import UserPointView
 from app.views.api.user.signup import UserSignupView
 
 blueprint = Blueprint('user-api', '/user')
@@ -10,3 +11,4 @@ blueprint.add_route(UserSignupView.as_view(), '/signup')
 
 blueprint.add_route(UserInformationView.as_view(), '/<username>')
 blueprint.add_route(UserMoneyView.as_view(), '/<username>/money')
+blueprint.add_route(UserPointView.as_view(), '/<username>/point')
