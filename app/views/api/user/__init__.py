12a1d66cc38b0f91998eb48a83b958ca2c900655
Moreal <dev.moreal@gmail.com>
2019-07-24 16:33:03 +0900

    update: repository -> service in views

--- app/views/api/user/__init__.py
+++ app/views/api/user/__init__.py
@@ -1,16 +1,19 @@
 from sanic import Blueprint
+from sanic.exceptions import abort
 from sanic.response import json
 from sanic.views import HTTPMethodView
 
 from app.repositories.connections import MySQLConnection
 from app.repositories.user import UserRepository
+from app.services.user import UserService
 from app.views.api.user.money import UserMoneyView
 
 class UserView(HTTPMethodView):
     repository = UserRepository(MySQLConnection)
+    service = UserService(repository)
 
     async def get(self, request, username: int):
-        user = await self.repository.get(username)
+        user = await self.service.get(username)
         del user['password']
         return json(user)
 
--- app/views/api/user/money.py
+++ app/views/api/user/money.py
@@ -4,19 +4,21 @@ from sanic.views import HTTPMethodView
 
 from app.repositories.connections import MySQLConnection
 from app.repositories.user import UserRepository
+from app.services.user import UserService
 
 
 class UserMoneyView(HTTPMethodView):
     repository = UserRepository(MySQLConnection)
+    service = UserService(repository)
 
     async def get(self, request, username: str):
-        user = await self.repository.get(username)
+        user = await self.service.get(username)
         return json({
             'money': user['money']
         })
 
     async def post(self, request: Request, username: str):
-        user = await self.repository.get(username)
+        user = await self.service.get(username)
         user['money'] += request.json['money']
         await self.repository.patch(
             username,
