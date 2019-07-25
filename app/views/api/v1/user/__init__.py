7e9134311fe5e60f22fa0e7b9dce44d9b5112034
Moreal <dev.moreal@gmail.com>
2019-07-25 14:38:18 +0900

    refactor: remake information_view as blueprint

--- app/views/api/v1/user/__init__.py
+++ app/views/api/v1/user/__init__.py
@@ -5,13 +5,9 @@ from app.views.api.v1.user.friend import UserFriendsView
 from app.views.api.v1.user.information import UserInformationView
 from app.views.api.v1.user.signup import UserSignupView
 
-blueprint = Blueprint('user-api')
-
-# blueprint.add_route(UserSignupView.as_view(), '/signup')
-
-blueprint.add_route(UserInformationView.as_view(), '/')
-
-blueprint = Blueprint.group(blueprint,
-                            friend.blueprint,
-                            *patch.create_patchable_blueprints(),
-                            url_prefix='/user/<username>')
+blueprint = Blueprint.group(
+    friend.blueprint,
+    information.blueprint,
+    *patch.create_patchable_blueprints(),
+    url_prefix='/user/<username>'
+)
--- app/views/api/v1/user/information.py
+++ app/views/api/v1/user/information.py
@@ -1,9 +1,12 @@
+from sanic import Blueprint
 from sanic.response import json
 from sanic.views import HTTPMethodView
 
 from app.repositories.connections import MySQLConnection
 from app.repositories.user import UserRepository
 from app.services.user import UserService
+
+blueprint = Blueprint('user-information-api', '/')
 
 class UserInformationView(HTTPMethodView):
     repository = UserRepository(MySQLConnection)
@@ -13,3 +16,5 @@ class UserInformationView(HTTPMethodView):
         user = await self.service.get(username)
         del user['password']
         return json(user)
+
+blueprint.add_route(UserInformationView.as_view(), '/')
