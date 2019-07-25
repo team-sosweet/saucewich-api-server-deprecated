b96d0994df588622ba33ee6bfcd9415e1a9293cf
Moreal <dev.moreal@gmail.com>
2019-07-25 16:58:37 +0900

    update: the way to create view class
    sanic_openapi collect routes by handler. so we have to make new class, new function

--- app/views/api/v1/user/patch.py
+++ app/views/api/v1/user/patch.py
@@ -2,7 +2,6 @@ from typing import List
 
 from sanic import Blueprint
 from sanic.exceptions import abort
-from sanic.request import Request
 from sanic.response import json
 from sanic.views import HTTPMethodView
 
@@ -12,10 +11,37 @@ from app.services.user import UserService
 
 
 def create_patchable_blueprint(field: str) -> Blueprint:
-    blueprint = Blueprint(f'user-patch-{field}', url_prefix=f'/{field}')
+    blueprint = Blueprint(f'user-patch-{field}-api', url_prefix=f'/{field}')
 
-    class UserPatchView(UserPatchBaseView):
-        field_name = field
+    class UserPatchView(HTTPMethodView):
+        repository = UserRepository(MySQLConnection)
+        service = UserService(repository)
+
+        field_name: str = field
+
+        async def get(self, request, username: str):
+            field_name = self.field_name
+            if field_name not in self.repository.patchable_fields:
+                abort(404)
+            user = await self.service.get(username)
+            return json({
+                field_name: user[field_name]
+            })
+
+        async def post(self, request, username: str):
+            field_name = self.field_name
+            if field_name not in self.service.patchable_fields:
+                abort(403)
+            user = await self.service.get(username)
+            await self.repository.patch(
+                username,
+                {
+                    field_name: user[field_name] + request.json[field_name]
+                }
+            )
+            return json({
+                'success': True
+            })
 
     blueprint.add_route(UserPatchView.as_view(), '/')
 
@@ -26,34 +52,3 @@ def create_patchable_blueprints() -> List[Blueprint]:
     return list(
         map(create_patchable_blueprint, UserRepository.patchable_fields)
     )
-
-
-class UserPatchBaseView(HTTPMethodView):
-    repository = UserRepository(MySQLConnection)
-    service = UserService(repository)
-
-    field_name: str
-
-    async def get(self, request, username: str):
-        field_name = self.field_name
-        if field_name not in self.repository.patchable_fields:
-            abort(404)
-        user = await self.service.get(username)
-        return json({
-            field_name: user[field_name]
-        })
-
-    async def post(self, request, username: str):
-        field_name = self.field_name
-        if field_name not in self.service.patchable_fields:
-            abort(403)
-        user = await self.service.get(username)
-        await self.repository.patch(
-            username,
-            {
-                field_name: user[field_name] + request.json[field_name]
-            }
-        )
-        return json({
-            'success': True
-        })
