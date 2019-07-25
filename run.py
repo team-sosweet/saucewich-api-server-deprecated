9998308900a1fbd0f42deb7fb798632ec6fb51d6
Moreal <dev.moreal@gmail.com>
2019-07-25 16:07:26 +0900

    update: run.py to run by config RUN_SETTTING

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import create_app
 if __name__ == '__main__':
     app = create_app()
     app.config.API_URI_FILTER = 'slash'
-    app.run(debug=True)
+    app.run(**app.config['RUN_SETTING'])
