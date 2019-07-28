c7b2e594b3b79d7a70b6bb89e9944ebc5242eaf5
Moreal <dev.moreal@gmail.com>
2019-07-28 21:52:39 +0900

    update: deprecate /api/v1

--- app/views/api/v1/__init__.py
+++ app/views/api/v1/__init__.py
@@ -3,8 +3,5 @@ from sanic import Blueprint
 from app.views.api.v1 import user, signup, signin
 
 blueprint = Blueprint.group(
-    user.blueprint,
-    signin.blueprint,
-    signup.blueprint,
     url_prefix='/v1',
 )
