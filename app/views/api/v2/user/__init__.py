a8e1731b1543bb06c0105107a98fdf1c00ae553d
Moreal <dev.moreal@gmail.com>
2019-08-02 18:38:51 +0900

    update: deprecated dynamic actions and useless views

--- app/views/api/v2/user/__init__.py
+++ app/views/api/v2/user/__init__.py
@@ -2,15 +2,11 @@ from sanic import Blueprint
 
 from app.views.api.v2.user import (
     field,
-    friend,
-    friend_request,
     information
 )
 
 blueprint = Blueprint.group(
-    friend.blueprint,
-    friend_request.blueprint,
-    information.blueprint,
     *field.create_gettable_blueprints(),
+    information.blueprint,
     url_prefix='/user/<username>'
 )
